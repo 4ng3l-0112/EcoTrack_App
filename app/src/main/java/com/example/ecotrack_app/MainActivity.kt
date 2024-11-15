fd43fb0040d1ee34eeea87bc667fde284e1c5c23
lutang <kamsawenannexie@gmail.com>
2024-11-15 23:37:29 +0800

    asdfghjk

--- app/src/main/java/com/example/ecotrack_app/MainActivity.kt
+++ app/src/main/java/com/example/ecotrack_app/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.example.ecotrack_app
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
-import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
